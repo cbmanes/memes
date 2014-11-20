1a936393c7c02749e54cf080ba6a4048b6273ea8
cbmanes <chasebmaney@gmail.com>
2014-11-20 16:36:47 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,2 +1,2 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + "im an og nigga, all rights reserved lmao</p>";
+"<p>"im an og nigga, all rights reserved lmao</p>";

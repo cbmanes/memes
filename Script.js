388a18d72e92996a50c58d3cadac846c53a053fc
cbmanes <chasebmaney@gmail.com>
2014-11-20 16:30:48 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,2 +1,2 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + "im an og nigga, all rights reserved lmao</p>"<a href="http://www.cbmanes.github.io/memes" />;
+"<p>&copy;  " + new Date().getFullYear() + "im an og nigga, all rights reserved lmao</p>";

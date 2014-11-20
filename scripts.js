5870422e6e6e66176218cd3535915d7d461b3799
cbmanes <chasebmaney@gmail.com>
2014-11-20 16:48:12 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 document.getElementById("foot01").innerHTML =
 "<p>"im an og nigga, all rights reserved lmao</p>";
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<button onclick="myFunction('carl azus','meme overlord')">click for hardcore porn ;)</button>
+
+<p id="demo"></p>
+
+function myFunction(name,job) {
+    document.getElementById("demo").innerHTML =
+    "hello " + name + ", the " + job + ".";
+}

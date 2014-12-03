f6b232f67be5db827f2d407a5d6b669e74bb4ecd
cbmanes <chasebmaney@gmail.com>
2014-12-03 16:01:03 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,4 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + "carl azuz is satan</p>";
+"<p>&copy;  " + new Date().getFullYear() + "carl azuz is satan</p>", "<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+shit what time is it</button>";
